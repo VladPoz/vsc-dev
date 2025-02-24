42311a15babefe542f82e1a7ef35631fe4b83963
VladPoz <vladpozdey08@gmail.com>
2025-02-24 20:25:07 +0500

    3

--- Shop/index.js
+++ Shop/index.js
@@ -1,61 +1,3 @@
-const header = document.querySelector('header');
-const slider = document.querySelector('slider');
-const main = document.querySelector('main');
-const end = document.querySelector('end');
-const login = document.querySelector('login');
-
-const login__menu__button = document.querySelector('.login__menu__button')
-const login__or__register__h1 = document.querySelector('.login__or__register__h1')
-const register = document.querySelector('.register');
-const registers = document.querySelector('.registers');
-const close__btn = document.querySelector('.login__menu__close');
-const account = document.querySelector('#header__contents__mainContents__content__account');
-const password = document.querySelector('#login__menu__password');
-const password__checkbox = document.querySelector('.login__menu__hide__checkbox');
-
-close__btn.addEventListener('click', ()=>{
-    header.style.display = "block";
-    slider.style.display = "none";
-    main.style.display = "flex";
-    end.style.display = "none";
-    login.style.display = "none";
-});
-
-account.addEventListener('click', ()=>{
-    header.style.display = "none";
-    slider.style.display = "none";
-    main.style.display = "none";
-    end.style.display = "none";
-    login.style.display = "flex";
-});
-
-password__checkbox.addEventListener('change', ()=>{
-    if(password__checkbox.checked){
-        password.type = 'text';
-    }
-    else{
-        console.log("hz");
-        password.type = 'password';
-    }
-})
-
-register.addEventListener('click', ()=>{
-    
-    if(register.innerHTML == 'Register'){
-        register.innerHTML = 'Login';
-        registers.childNodes[0].nodeValue = 'I have an account | ';
-        login__or__register__h1.innerHTML = 'Register';
-        login__menu__button.innerHTML = 'Register';
-        
-    }
-    else{
-        register.innerHTML = 'Register';
-        registers.childNodes[0].nodeValue = "I don't have an account | ";
-        login__or__register__h1.innerHTML = 'Login';
-        login__menu__button.innerHTML = 'Login';
-    }
-})
-
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         const response = await fetch("main.json");
@@ -83,3 +25,22 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 });
 
+const login__menu__button = document.querySelector('.login__menu__button')
+const login__or__register__h1 = document.querySelector('.login__or__register__h1')
+const register = document.querySelector('.register');
+const registers = document.querySelector('.registers');
+const close__btn = document.querySelector('.login__menu__close');
+const account = document.querySelector('#header__contents__mainContents__content__account');
+const password = document.querySelectorAll('#login__menu__password');
+const password__checkbox = document.querySelector('.login__menu__hide__checkbox');
+
+password__checkbox.addEventListener('change', ()=>{
+    if(password__checkbox.checked){
+        password.type = 'text';
+        console.log("a");
+    }
+    else{
+        password.type = 'password';
+        console.log('J')
+    }
+})
